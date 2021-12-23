88ef209bca30e17a1003d55fe1bbca8aec6ebd98
kumarrittik99 <96478116+kumarrittik1999@users.noreply.github.com>
2021-12-23 17:34:34 +0530

    Bound Service

--- app/src/main/java/com/example/servicesimplementation/MainActivity.kt
+++ app/src/main/java/com/example/servicesimplementation/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.servicesimplementation
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.servicesimplementation.databinding.ActivityMainBinding
 
@@ -11,6 +15,10 @@ class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
 
     private lateinit var binding: ActivityMainBinding
+
+    private var isBound = false
+    private lateinit var serviceConnection: ServiceConnection
+    private lateinit var myService: MyService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,13 +34,68 @@ class MainActivity : AppCompatActivity() {
         binding.btnStopService.setOnClickListener {
             stopMyService(serviceIntent)
         }
+
+        serviceConnection = object : ServiceConnection {
+            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+                isBound = true
+                val binder = service as MyService.MyBinder
+                myService = binder.getMyService()
+            }
+
+            override fun onServiceDisconnected(name: ComponentName?) {
+                isBound = false
+            }
+
+        }
+
+        binding.btnBindService.setOnClickListener {
+            bindToMyService(serviceIntent)
+        }
+
+        binding.btnUnbindService.setOnClickListener {
+            unbindFromMyService(serviceIntent)
+        }
+
+        binding.btnGetData.setOnClickListener {
+            updateUI()
+        }
     }
 
     private fun startMyService(intent: Intent) {
         startService(intent)
+        Toast.makeText(this, "Service started succesfully!", Toast.LENGTH_SHORT).show()
     }
 
     private fun stopMyService(intent: Intent) {
         stopService(intent)
+        Toast.makeText(this, "Service stopped succesfully!", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun bindToMyService(intent: Intent) {
+        if(!isBound) {
+            bindService(intent, serviceConnection, BIND_AUTO_CREATE)
+            Toast.makeText(this, "Service bound succesfully!", Toast.LENGTH_SHORT).show()
+            isBound = true
+            return
+        }
+
+        Toast.makeText(this, "Already bound to the service!", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun unbindFromMyService(intent: Intent) {
+        if(isBound) {
+            unbindService(serviceConnection)
+            Toast.makeText(this, "Service unbound succesfully!", Toast.LENGTH_SHORT).show()
+            isBound = false
+            return
+        }
+
+        Toast.makeText(this, "Service already unbounded!", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun updateUI() {
+        if(isBound) {
+            binding.tvRandomNumber.text = myService.getRandomNumber().toString()
+        }
     }
 }
--- app/src/main/java/com/example/servicesimplementation/MyService.kt
+++ app/src/main/java/com/example/servicesimplementation/MyService.kt
@@ -2,12 +2,16 @@ package com.example.servicesimplementation
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 import android.util.Log
 
 class MyService : Service() {
 
     private val TAG = "MyService"
+
+    private var isBound = false
+    private var generatedRandomNumber = 0
 
     override fun onCreate() {
         Log.i(TAG, "Thread running on " + Thread.currentThread().name)
@@ -21,10 +25,44 @@ class MyService : Service() {
 
     override fun onBind(intent: Intent): IBinder {
         Log.i(TAG, "onBind")
-        TODO("Return the communication channel to the service.")
+        isBound = true
+        Thread(Runnable {
+            generateRandomNumber()
+        }).start()
+        return MyBinder()
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        Log.i(TAG, "onUnbind")
+        isBound = false
+        return super.onUnbind(intent)
     }
 
     override fun onDestroy() {
         Log.i(TAG, "onDestroy")
     }
+
+    private fun generateRandomNumber() {
+        while(isBound) {
+            Thread.sleep(1000)
+            generatedRandomNumber = (0..100).random()
+        }
+    }
+
+    fun getRandomNumber(): Int {
+        return generatedRandomNumber
+    }
+
+    /*
+        We return an IBinder interface object from OnBind method which helps in intercommunication
+        between component bound to the service and the service
+
+        We expose a method from the Binder class which returns the instance of MyService
+    */
+    inner class MyBinder : Binder() {
+        fun getMyService(): MyService {
+            return this@MyService
+        }
+    }
+
 }
